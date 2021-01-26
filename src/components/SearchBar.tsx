7cf6a0f42af3f83fa6deeba4e328e4da626218ec
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-26 23:08:07 +0300

    styled searchbar TextInput

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { TextInput } from "react-native-gesture-handler";
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
-      <Text>SearchBar SearchBar</Text>
+      <Feather name="search" size={35} />
+      <TextInput style={styles.input} placeholder="Search" />
     </View>
   );
 };
@@ -13,9 +16,19 @@ export default SearchBar;
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: "#cccbcb",
+    backgroundColor: "#e5e2e2",
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 10,
+    marginTop: 15
+  },
+  input: {
+    height: 50,
+    paddingLeft: 15,
+    fontSize: 18,
+    flex: 1
   }
 });
