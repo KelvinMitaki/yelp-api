3c76ef53d47d31436241eab1500f4f8758cfb3e4
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-27 14:21:52 +0300

    added showing images in ResultsShowScreen

--- src/screens/ResultsShowScreen.tsx
+++ src/screens/ResultsShowScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { NavigationRoute } from "react-navigation";
 import { StackNavigationProp } from "react-navigation-stack/lib/typescript/src/vendor/types";
 import yelp from "../api/yelp";
@@ -61,20 +62,50 @@ export interface BusinessDetails {
 const ResultsShowScreen: React.FC<{
   navigation: StackNavigationProp<NavigationRoute, { id: string }>;
 }> = props => {
+  const [details, setDetails] = useState<BusinessDetails | null>(null);
   useEffect(() => {
     resultsDetails(props.navigation.state.params!.id);
   }, []);
   const resultsDetails = async (id: string) => {
     const { data } = await yelp.get(`/${id}`);
-    console.log(data);
+    setDetails(data);
   };
   return (
     <View>
-      <Text>ResultsShowScreen ResultsShowScreen</Text>
+      {details && (
+        <View>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 20,
+              alignSelf: "center",
+              marginVertical: 10
+            }}
+          >
+            Gallery
+          </Text>
+          <FlatList
+            data={details.photos}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+              <Image source={{ uri: item }} style={styles.img} />
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ResultsShowScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  img: {
+    height: 150,
+    width: 150,
+    borderRadius: 5,
+    marginHorizontal: 10
+  }
+});
